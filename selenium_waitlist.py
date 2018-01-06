a62e30d03d9882c8420aff17e8cee837b1f2c78f
James Angeles <JamesAngeles21@gmail.com>
2018-01-06 15:51:51 -0500

    isolated each course object in array and naviagated to appropiate course options

--- selenium_waitlist.py
+++ selenium_waitlist.py
@@ -20,9 +20,83 @@ driver.get("https://psns.cc.stonybrook.edu/psc/csprods/EMPLOYEE/CAMP/c/PRJCS_MEN
 open_builder = driver.find_element_by_id("PRJCS_DERIVED_PRJCS_LAUNCH_CS")
 open_builder.click()
 
-sleep(5)
+sleep(8)
+
+if len(driver.window_handles) < 2:
+	sleep(10)
 
 driver.switch_to.window(driver.window_handles[1])
 
 driver.get("https://stonybrook.collegescheduler.com/terms/Spring%202018/options")
-shopping_cart = driver.find_elements_by_class_name("section-item")
+sleep(2)
+
+shopping_cart = driver.find_elements_by_class_name("shopping-cart")
+
+
+def parse_shopping_cart(shopping_cart):
+
+	shopping_cart_strings = []
+
+	for item in shopping_cart:
+		shopping_cart_strings.append(item.text)
+
+	shopping_cart_strings = shopping_cart_strings[0].split('\n')
+	shopping_cart_strings = shopping_cart_strings[2:]
+	return shopping_cart_strings
+
+
+def create_shopping_item(subject,setting):
+
+	shopping_item = {
+		"subject" : subject,
+		"setting" : setting
+	}
+
+	return shopping_item
+
+def parse_shopping_items(shopping_strings):
+
+	shopping_items = []
+
+	for index in range(len(shopping_strings)):
+
+		if (index + 1) % 3 == 0:
+			subject = shopping_strings[index-2][:7]
+			setting = shopping_strings[index]
+			shopping_items.append(create_shopping_item(subject,setting))
+
+	return shopping_items
+
+
+shopping_strings = parse_shopping_cart(shopping_cart)
+shopping_items = parse_shopping_items(shopping_strings)
+
+for item in shopping_items:
+	print item['subject'], item['setting']
+
+
+
+
+def find_correct_course(shopping_item):
+	course = shopping_item['subject']
+	xpath = '//*[strong= "'+ str(course) + '"]'
+	course_name = driver.find_element_by_xpath(xpath)
+	course_item = course_name.find_element_by_xpath("..")
+	course_id = course_item.get_attribute("data-id")
+	driver.get("https://stonybrook.collegescheduler.com/terms/Spring%202018/courses/" + str(course_id))
+
+
+find_correct_course(shopping_items[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
